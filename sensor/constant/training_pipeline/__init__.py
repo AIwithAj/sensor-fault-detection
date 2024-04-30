ae3a9fd5e75a328b39499499f3a885fc6f8c9ee5
Ajay <dancerworld60@gmail.com>
2024-04-30 12:07:15 +0530

    data validation component completed

--- sensor/constant/training_pipeline/__init__.py
+++ sensor/constant/training_pipeline/__init__.py
@@ -1,5 +1,8 @@
 import os
+from sensor.constant.s3_bucket import TRAINING_BUCKET_NAME
 
+SAVED_MODEL_DIR =os.path.join("saved_models")
+# defining common constant variable for training pipeline
 TARGET_COLUMN = "class"
 PIPELINE_NAME: str = "sensor"
 ARTIFACT_DIR: str = "artifact"
@@ -23,3 +26,12 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
 
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,17 +1,19 @@
-
-from sensor.entity.artifact_entity import DataIngestionArtifact
-from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
+from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig
+from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 from sensor.exception import SensorException
 import sys,os
 from sensor.logger import logging
 from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
 
+
+from sensor.constant.training_pipeline import SAVED_MODEL_DIR
 
 class TrainPipeline:
-    is_pipeline_running=False
+
     def __init__(self):
         self.training_pipeline_config = TrainingPipelineConfig()
-        
+   
 
 
     def start_data_ingestion(self)->DataIngestionArtifact:
@@ -25,11 +27,29 @@ class TrainPipeline:
         except  Exception as e:
             raise  SensorException(e,sys)
 
+    def start_data_validaton(self,data_ingestion_artifact:DataIngestionArtifact)->DataValidationArtifact:
+        try:
+            data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+            data_validation_config = data_validation_config
+            )
+            data_validation_artifact = data_validation.initiate_data_validation()
+            return data_validation_artifact
+        except  Exception as e:
+            raise  SensorException(e,sys)
+
+   
 
     def run_pipeline(self):
         try:
+            
+        
 
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
+            data_validation_artifact=self.start_data_validaton(data_ingestion_artifact=data_ingestion_artifact)
+           
+ 
+     
         except  Exception as e:
-    
+     
             raise  SensorException(e,sys)
